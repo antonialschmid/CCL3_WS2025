46734048e552ce4d4bf18f336b10cdc9e42c07db
kathrin <cc241016@fhstp.ac.at>
2026-01-14 13:25:39 +0100

    import fixes

--- code/app/src/main/java/com/example/gratitudegarden/screen/SettingsScreen.kt
+++ code/app/src/main/java/com/example/gratitudegarden/screen/SettingsScreen.kt
@@ -2,6 +2,8 @@ package com.example.gratitudegarden.screen
 
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Settings
